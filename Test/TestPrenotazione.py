242cebcce84def6d1e007edb9d31562494ddaddc
Simone Sgalla <simonesgalla@libero.it>
2022-09-03 12:12:56 +0200

    aa

--- Test/TestPrenotazione.py
+++ Test/TestPrenotazione.py
@@ -1,6 +1,7 @@
 import datetime
 from unittest import TestCase
 
+from Amministrazione.Segreteria import Segreteria
 from Amministrazione.Sistema import Sistema
 from Servizio.Prenotazione import Prenotazione
 
@@ -11,9 +12,19 @@ class TestPrenotazione(TestCase):
         self.prenotazione=Prenotazione()
         self.prenotazione.cliente='prova'
         self.prenotazione.dataOra=datetime.datetime.now()
-        self.prenotazione.stampaPrenotazione()
         sistema=Sistema([])
         sistema.listaPrenotazioni=[self.prenotazione]
         sistema.salvaPrenotazioni()
         sistema.leggiPrenotazioni()
         self.assertEqual(self.prenotazione,sistema.listaPrenotazioni[0])
+
+    def testEliminaPrenotazione(self):
+        self.prenotazione = Prenotazione()
+        self.prenotazione.cliente = 'prova'
+        self.prenotazione.dataOra = datetime.datetime.now()
+        segreteteria = Segreteria()
+        segreteteria.x.listaPrenotazioni = [self.prenotazione]
+        segreteteria.eliminaPrenotazione(self.prenotazione)
+        segreteteria.x.leggiPrenotazioni()
+        for prenotazione in segreteteria.x.listaPrenotazioni:
+            self.assertNotEqual(prenotazione,self.prenotazione)
